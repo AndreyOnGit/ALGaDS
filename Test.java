7d699e37d88d18b44ab86ca5f9bfda2fdd202797
AndreyOnGit <lacosh@gmail.com>
2021-12-30 23:31:49 +0300

    hw_6

--- Test.java
+++ Test.java
@@ -1,38 +1,49 @@
 package lesson6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
+        int quantity = 20;
 
-        Tree<Integer> tree = new TreeImpl<>();
+        List<TreeImpl> treeList = new ArrayList<>();
+        while (treeList.size() < quantity) {
+            TreeImpl<Integer> tree = new TreeImpl<>();
+            makeTree(tree);
+            treeList.add(tree);
+        }
 
-        tree.add(60);
-        tree.add(50);
-        tree.add(66);
-        tree.add(40);
-        tree.add(55);
-        tree.add(70);
-        tree.add(31);
-        tree.add(45);
-        tree.add(42);
-        tree.add(43);
-        tree.add(69);
-        tree.add(67);
-        tree.add(68);
-        tree.add(81);
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Дерево №" + i + ". Балансировка: " + isBalanced(treeList.get(i).getRoot()));
+            treeList.get(i).display();
+        }
+    }
 
-        tree.display();
-//
-//        tree.remove(31);
-//        tree.remove(42);
-//        tree.remove(70);
-//        tree.remove(40);
-        tree.remove(60);
-//
-        tree.display();
-//
-        tree.traverse(Tree.TraversMode.IN_ORDER);
-        tree.traverse(Tree.TraversMode.PRE_ORDER);
-        tree.traverse(Tree.TraversMode.POST_ORDER);
+    public static void makeTree(Tree<Integer> tree) {
+        int min = -25;
+        int max = 25;
+        int value;
+        int count = 0;
 
+        max -= min;
+        max++;
+
+        while (count < 20) {
+            value = (int) (Math.random() * max) + min;
+            tree.add(value);
+            count++;
+        }
+    }
+
+    public static boolean isBalanced(Node node) {
+        return (node == null) ||
+                isBalanced(node.getLeftChild()) &&
+                        isBalanced(node.getRightChild()) &&
+                        Math.abs(height(node.getLeftChild()) - height(node.getRightChild())) <= 1;
+    }
+
+    private static int height(Node node) {
+        return node == null ? 0 : 1 + Math.max(height(node.getLeftChild()), height(node.getRightChild()));
     }
 }
